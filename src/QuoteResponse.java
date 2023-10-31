9a01e0942dd31a0203980ab762df267f909e1cb6
Matthew Meyer <matt.meyer997@gmail.com>
2023-10-31 13:23:18 -0400

    Create QuoteResponse.java

---
+++ src/QuoteResponse.java
@@ -0,0 +1,80 @@
+import java.time.LocalDateTime;
+
+public class QuoteResponse {
+	private int responseID;
+	private int quoteID;
+	private double modifiedPrice;
+	private LocalDateTime modifiedStartTime;
+	private LocalDateTime modifiedEndTime;
+	private String note;
+	private LocalDateTime createdAt;
+	
+	public QuoteResponse() { }
+	
+	public QuoteResponse(int responseID, int quoteID, double modifiedPrice, LocalDateTime modifiedStartTime, 
+			LocalDateTime modifiedEndTime, String note, LocalDateTime createdAt) {
+		this.responseID = responseID;
+		this.quoteID = quoteID;
+		this.modifiedPrice = modifiedPrice;
+		this.modifiedStartTime = modifiedStartTime;
+		this.modifiedEndTime = modifiedEndTime;
+		this.note = note;
+		this.createdAt = createdAt;
+	}
+
+	public int getResponseID() {
+		return responseID;
+	}
+
+	public void setResponseID(int responseID) {
+		this.responseID = responseID;
+	}
+
+	public int getQuoteID() {
+		return quoteID;
+	}
+
+	public void setQuoteID(int quoteID) {
+		this.quoteID = quoteID;
+	}
+
+	public double getModifiedPrice() {
+		return modifiedPrice;
+	}
+
+	public void setModifiedPrice(double modifiedPrice) {
+		this.modifiedPrice = modifiedPrice;
+	}
+
+	public LocalDateTime getModifiedStartTime() {
+		return modifiedStartTime;
+	}
+
+	public void setModifiedStartTime(LocalDateTime modifiedStartTime) {
+		this.modifiedStartTime = modifiedStartTime;
+	}
+
+	public LocalDateTime getModifiedEndTime() {
+		return modifiedEndTime;
+	}
+
+	public void setModifiedEndTime(LocalDateTime modifiedEndTime) {
+		this.modifiedEndTime = modifiedEndTime;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+}
