142a7805769220bd29f671abcfd7e356e31b01a2
Matthew Meyer <matt.meyer997@gmail.com>
2023-10-31 12:52:44 -0400

    Removed commented out imports.

--- src/QuoteDAO.java
+++ src/QuoteDAO.java
@@ -14,10 +14,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-//import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
