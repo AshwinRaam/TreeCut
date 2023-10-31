f35ddfd5f8d46318c3e2ff27cbd0b46fe9dcc41d
Matthew Meyer <37270280+commandobox@users.noreply.github.com>
2023-10-30 21:41:00 -0400

    Create QuoteDAO.java

---
+++ src/QuoteDAO.java
@@ -0,0 +1,41 @@
+
+public class QuoteDAO {
+	private static final long serialVersionUID = 1L;
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	public QuoteDAO() {}
+	
+	/** 
+	 * @see HttpServlet#HttpServlet()
+     */
+    protected void connect_func() throws SQLException {
+    	//uses default connection to the database
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/TreeCut?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false&user=john&password=pass1234");
+            System.out.println(connect);
+        }
+    }
+    
+	//connect to the database 
+    public void connect_func(String username, String password) throws SQLException {
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager
+  			      .getConnection("jdbc:mysql://127.0.0.1:3306/userdb?"
+  			          + "useSSL=false&user=" + username + "&password=" + password);
+            System.out.println(connect);
+        }
+    }
+}
