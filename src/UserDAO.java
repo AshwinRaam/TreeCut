805cd69d5559688a476b9d81124879f5110bda5f
Ashwin Raam <raamocyby@gmail.com>
2023-11-09 13:06:09 -0500

    UPDATES UserDAOC

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -3,20 +3,15 @@ import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-//import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
