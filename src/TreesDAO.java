38c8892a98cadb1d7145e3c2277c991be2f65b08
Matthew Meyer <37270280+commandobox@users.noreply.github.com>
2023-11-14 00:02:42 -0500

    Create TreesDAO.java

---
+++ src/TreesDAO.java
@@ -0,0 +1,182 @@
+import java.io.*;
+import java.sql.*;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.Part;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/treesDAO")
+public class TreesDAO {
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public TreesDAO() { }
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    protected void connect_func() throws SQLException {
+        //uses default connection to the database
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/treecut?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false&user=john&password=pass1234");
+            System.out.println(connect);
+        }
+    }
+
+    //connect to the database
+    public void connect_func(String username, String password) throws SQLException {
+        if (connect == null || connect.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            connect = (Connection) DriverManager
+                    .getConnection("jdbc:mysql://127.0.0.1:3306/treecut?"
+                            + "useSSL=false&user=" + username + "&password=" + password);
+            System.out.println(connect);
+        }
+    }
+
+    /**
+     * Because I'm too lazy to keep copying and pasting code, send the result set here.
+     * @param resultSet
+     * @return A new tree object.
+     * @throws SQLException
+     */
+    private Tree createTree(ResultSet resultSet) throws SQLException {
+        int treeID = resultSet.getInt("treeID");
+        int quoteID = resultSet.getInt("quoteID");
+        String size = resultSet.getString("size");
+        String height = resultSet.getString("height");
+        String location = resultSet.getString("location");
+        String nearHouse = resultSet.getString("nearHouse");
+        String pictureURL1 = resultSet.getString("pictureURL1");
+        String pictureURL2 = resultSet.getString("pictureURL2");
+        String pictureURL3 = resultSet.getString("pictureURL3");
+        Timestamp createdAt = resultSet.getTimestamp("createdAt");
+        Timestamp updatedAt = resultSet.getTimestamp("updatedAt");
+
+        return new Tree(
+                treeID,
+                quoteID,
+                size,
+                height,
+                location,
+                nearHouse,
+                pictureURL1,
+                pictureURL2,
+                pictureURL3,
+                createdAt,
+                updatedAt
+        );
+    }
+
+    public Tree getTree(int treeID) throws SQLException {
+        String sql = "SELECT * FROM trees WHERE quoteID = ?";
+
+        connect_func();
+        preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.setInt(1, treeID);
+        resultSet = preparedStatement.executeQuery();
+
+        Tree tree = new Tree();
+        while(resultSet.next()) {
+            tree = createTree(resultSet);
+        }
+
+        return tree;
+    }
+
+    public List<Tree> getTrees(int quoteID) throws SQLException {
+        String sql = "SELECT * FROM trees WHERE quoteID = ?";
+
+        connect_func();
+        preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.setInt(1, quoteID);
+        resultSet = preparedStatement.executeQuery();
+
+        List<Tree> trees = new ArrayList<Tree>();
+        while(resultSet.next())
+        {
+            createTree(resultSet);
+        }
+        return trees;
+    }
+
+    public void insertTree(Tree tree) throws SQLException {
+        String sql = "INSERT INTO trees (quoteID, size, height, location, " +
+                "nearhouse, pictureURL1, pictureURL2, pictureURL3, createdAt) " +
+                "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, NOW())";
+
+        connect_func();
+        preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.setInt(1, tree.getQuoteID());
+        preparedStatement.setString(2, tree.getSize());
+        preparedStatement.setString(3, tree.getHeight());
+        preparedStatement.setString(4, tree.getLocation());
+        preparedStatement.setString(5, tree.getNearHouse());
+        preparedStatement.setString(6, tree.getPictureURL1());
+        preparedStatement.setString(7, tree.getPictureURL2());
+        preparedStatement.setString(8, tree.getPictureURL3());
+        preparedStatement.execute();
+    }
+
+    public void InsertTrees(List<Tree> trees) throws SQLException {
+        for(Tree tree : trees) {
+            insertTree(tree);
+        }
+    }
+
+    /**
+     * Save the image of a tree to the server via the Part of the image.
+     * @param imagePart
+     * @return The String of the location the image is saved at.
+     * @throws IOException
+     */
+    public String saveUploadedImage(Part imagePart) throws IOException {
+        String path = "." + File.separator + "images";
+        String fileName = getFileName(imagePart); //better hope there isnt a file of the same name
+        String fullPath = path + File.separator + fileName;
+
+        File directory = new File(path);
+        if (!directory.exists())
+            directory.mkdirs();
+        File file = new File(fullPath);
+        OutputStream out = new FileOutputStream(new File(fullPath));
+        InputStream fileContent = imagePart.getInputStream();
+        int read = 0;
+        final byte[] bytes = new byte[1024];
+
+        while ((read = fileContent.read(bytes)) != -1) {
+            out.write(bytes, 0, read);
+        }
+        System.out.println("File " + fileName + " created/updated at " + file.getCanonicalPath());
+        out.close();
+        fileContent.close();
+
+        return fullPath;
+    }
+
+    private String getFileName(final Part part) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+}
